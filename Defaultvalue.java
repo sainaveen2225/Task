ef6d200c144fe3412db6fce4179d37d4ad49cad1
sainaveen1807 <sai2010naveen@gmail.com>
2023-02-27 18:15:33 +0530

    Default values of data types

---
+++ Defaultvalue.java
@@ -0,0 +1,25 @@
+class Defaultvalue
+{
+	static boolean a;
+	static byte b;
+	static char ch;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static String str;
+	public static void main(String args[])
+	{
+	System.out.println("default value of boolean is\t"+a);
+	System.out.println("default value of byte is\t"+b);
+	System.out.println("default value of char is\t"+ch);
+	System.out.println("default value of short is\t"+s);
+	System.out.println("default value of int is \t"+i);
+	System.out.println("default value of long is\t"+l);
+	System.out.println("default value of float is\t"+f);
+	System.out.println("default value of double is\t"+d);
+	System.out.println("default value of string is\t"+str);
+	}
+}
+	
