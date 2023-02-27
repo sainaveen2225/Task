c8b80c867d3a6169daa1961b2633164d6ca2ae84
sainaveen1807 <sai2010naveen@gmail.com>
2023-02-27 17:52:38 +0530

    Arthmetic operations of given numbers

---
+++ Arithmetic.java
@@ -0,0 +1,19 @@
+class Arithmetic
+{
+	public static void main(String args[])
+	{
+	int add,substract,multiply;
+	float division;
+	int number1=4,number2=2;
+	add=number1+number2;
+	substract=number1-number2;
+	multiply=number1*number2;
+	division=number1/number2;
+	System.out.println(add);
+	System.out.println(substract);
+	System.out.println(multiply);
+	System.out.println(division);
+	}
+}
+
+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
