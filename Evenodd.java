f78c2d47893494d7fd82715303dd513bd025b063
sainaveen1807 <sai2010naveen@gmail.com>
2023-02-27 13:26:56 +0530

    To find out the given number is Even or Odd

---
+++ Evenodd.java
@@ -0,0 +1,16 @@
+/*To find the given number is even or odd*/
+class Evenodd
+{
+	public static void main(String as[])
+	{
+	int i=2;
+	if(i%2==0)
+	{
+		System.out.println("Given number is even");
+	}
+	else
+	{
+		System.out.println("Given number is odd");
+	}
+	}
+} 
